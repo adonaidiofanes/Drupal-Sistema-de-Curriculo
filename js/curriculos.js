2faede877f510034e22112642db8f22563bc9c63
adonai <adonaibm@gmail.com>
2015-04-05 22:13:23 -0300

    Adicionando funcao colorbox ao clicar no botao enviar curriculo

--- js/curriculos.js
+++ js/curriculos.js
@@ -3,7 +3,22 @@
 (function ($) {
 
 	$(function() {
-		
+
+		var carregando = "<div style='text-align:center;'><img src='" + Drupal.settings.basePath + "sites/all/modules/custom/curriculos/img/carregando.gif'><h1>ENVIANDO SEU CURRÍCULO!<BR>AGUARDE!</h1></div>";
+
+		$("#enviar_cv").on('click',function(){
+
+			$.colorbox({
+				html: carregando,
+				escKey: false, 
+				overlayClose: false, 
+				closeButton: false,
+				width: 500,
+				scrolling: false}
+			);
+
+		});
+
 	}) // fim da funcao jquery
 
 })(jQuery);
